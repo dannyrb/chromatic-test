6b980689f32eaae0970a31561077a69ae0cb1da6
Danny Brown <danny.brown@upstart.com>
2021-03-15 12:07:12 -0400

    get chromatic to work

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
@@ -7,5 +9,32 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: 'svg-url-loader',
+          options: {
+            limit: 10000,
+          },
+        },
+      ],
+    });
+
+    // Return the altered config
+    return config;
+  },
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+# Chromatic Test
+
+Note: Had to apply this 
+
+## ENV VARS
+
+`CHROMATIC_PROJECT_TOKEN` - Set in CI. Used by NPM Script: `yarn run chromatic`. Links build to chromatic project.
